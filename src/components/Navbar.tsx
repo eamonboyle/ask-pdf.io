4171e6db6a70a571c4e7331500a8f154782ee67f
Eamon Boyle <blaowskate@hotmail.com>
2023-09-28 22:16:17 +0100

    Inital Setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import MaxWithWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import { CreateOrgLink, LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+                <MaxWithWrapper>
+                    <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                        <Link href="/" className="flex z-40 font-semibold">
+                            <span>AskPDF.</span>
+                        </Link>
+
+                        { /* TODO: Add mobile menu */}
+
+                        <div className='hidden items-center space-x-4 sm:flex'>
+                            <>
+                                <Link
+                                    href='/pricing'
+                                    className={buttonVariants({
+                                        variant: 'ghost',
+                                        size: 'sm',
+                                    })}>
+                                    Pricing
+                                </Link>
+
+                                <LoginLink
+                                    className={buttonVariants({
+                                        variant: 'ghost',
+                                        size: 'sm',
+                                    })}>
+                                    Sign in
+                                </LoginLink>
+                                <RegisterLink
+                                    className={buttonVariants({
+                                        size: 'sm',
+                                    })}>
+                                    Get Started <ArrowRight className="ml-1.5 h-5 w-5" />
+                                </RegisterLink>
+                            </>
+                        </div>
+                    </div>
+                </MaxWithWrapper>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
